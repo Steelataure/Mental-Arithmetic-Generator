6c1925eef0d3160c6ecc297408ecd5b4cc9c1d7d
Steelataure <alexandre0312@orange.fr>
2022-11-15 20:08:32 +0100

    uml

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ noir = "black"
 blanc = "#f4f4f4"
  
 
-class Application(tk.Tk):
+class Programme(tk.Tk):
     def __init__(self, largeur=720, hauteur=630):
         tk.Tk.__init__(self)
 
+        self.iconbitmap('logo.ico')
         self.resizable(False, False)
         self.title("Générateur de calcul mental")
         self.largeur = largeur
@@ -30,7 +31,7 @@ class Application(tk.Tk):
         self.operateur_choice = random.choice(self.operateur)
         operateur_choice = self.operateur_choice
 
-        self.WIN = 0
+        self.LOSS = None
 
     def generate_operation(self):
         all_operation = str(self.pre_number) + self.operateur_choice + str(self.post_number)
@@ -55,7 +56,6 @@ class Application(tk.Tk):
             self.LOSS = 1
 
 
-
     def getReponseUser(self):
         self.input_value = self.my_input.get()
         return self.input_value
@@ -63,7 +63,7 @@ class Application(tk.Tk):
 
     def affichage(self):
 
-        self.background = PhotoImage(file = "apercu3.png")
+        self.background = PhotoImage(file = "assets/apercu3.png")
         tk.Label(self, image = self.background).place(x = -2, y = -1)
 
         self.my_label = Label(self, text="Veuillez résoudre l'opération", fg="green", bg=blanc, font=("Franklin Gothic Demi", 18)).place(x=205, y=205)
@@ -73,39 +73,19 @@ class Application(tk.Tk):
 
         self.my_input = tk.Entry(self, width= 27, font=("Franklin Gothic Demi", 18))
         self.my_input.place(x=162, y=310, height = 40)
-        #self.correction = tk.Label(self.root, text="Voici la correction:", fg="green", bg=blanc, font=("Franklin Gothic Demi", 18)).place(x=300, y=400)
 
-        self.Start_image = PhotoImage(file='start.png')
+        self.Start_image = PhotoImage(file='assets/start.png')
         self.Button_start = tk.Button(self, image=self.Start_image, borderwidth=0, bg=blanc, activebackground='WHITE', command=lambda:[self.getReponseUser(), self.win_condition()]).place(x=210, y=420)
-
-
-"""class Compteur(): 
-    def __init__(self, compteur_start = 10, compteur_stop = 0):
-
-        self.compteur_start = compteur_start
-        self.compteur_stop = compteur_stop
-
-    def compteur_avancement(self):
-        while self.compteur_start >= self.compteur_stop:
-
-            self.affichage['text'] = str(self.compteur_start)
-            self.compteur += 1
-            root.affichage.update_idletasks()
-            time.sleep(1)
-            return self """
             
 
 
 class User(): 
-    def __init__(self):
-        pass
-
+    def __init__(self, pseudo):
+        self.pseudo = pseudo
         
 
 if __name__ == "__main__":
 
-    root = Application()
+    root = Programme()
     root.affichage()
     root.mainloop()
-
-    print(root.verif_operation())
